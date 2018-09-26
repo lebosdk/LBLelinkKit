0263a92e3f385d12a69856c207934cdad8805029
aolingli <anl@hpplay.cn>
2018-09-26 17:37:00 +0800

    modify version.sh

--- version.sh
+++ version.sh
@@ -5,6 +5,7 @@ new_version=$1
 [ "x$new_version" == "x" ] && echo "please input new version and exit" && exit 1
 sed -i .tmp s/$old_version/$new_version/g LBLelinkKit.podspec #ios中的sed需要一个临时文件来转换
 [ $? -ne 0 ] && echo "must run at ios system and so exit" && exit 1
+rm *.tmp
 git diff LBLelinkKit.podspec
 git add LBLelinkKit.podspec
 git commit -m "modify to version $new_version"
